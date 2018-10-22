f811a0c19adc186ebdcfd449fe6b32bc574f9b8d
Akigawa <sonoskeakigawa@gmail.com>
2018-10-22 13:14:39 -0700

    tritracker

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var tri1 = 0;
+var tri2 = 0;
+var tri3 = 0;
+function calctriangle()
+{
+  console.log(tri1 + tri2 + tri3);
+  tri1 = $("#tri1").val();
+  tri2  = $("#tri2").val();
+  tri3  = $("#tri3").val();
+
+  if(tri1 == tri2 == tri3)
+  {
+    console.log(tri1 + tri2 + tri3);
+    /* Equilateral  */
+    tritype = "Equilateral";
+    $(".triangle").html(tritype);
+  }
+
+  else if(tri1 === tri2 != tri3 ||  tri1 != tri3 == tri2 || tri2 != tri1 == tri3)
+
+  {
+
+    console.log(tri1 + tri2 + tri3);
+    /* Isosceles  */
+    tritype = "isosceles";
+    $(".triangle").html(tritype);
+  }
+
+  else if(tri1 !== tri2 !== tri3)
+  {
+    console.log(tri1 + tri2 + tri3);
+    tritype = "scalene";
+    $(".triangle").html(tritype);
+  }
+
+}
+$(document).ready(function(){
+
+  });
