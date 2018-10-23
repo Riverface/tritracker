fdc409b6ab18703e682ebf682d7667722a5a19c6
Akigawa <sonoskeakigawa@gmail.com>
2018-10-23 13:12:25 -0700

    A A A A A A A A A A A

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,22 @@ function calctriangle()
   tri1 = $("#tri1").val();
   tri2  = $("#tri2").val();
   tri3  = $("#tri3").val();
+  if((tri1 + tri2) < tri3 || tri1 > (tri2 + tri3) || (tri1 + tri3) < tri2 || (tri1 || tri2 || tri3) == 0 || (tri1 || tri2 || tri3) == NaN)
+  {
+  tritype = "not a triangle"
+      $(".triangle").html(tritype);
+  }
+    else if(tri1 === tri2 && tri1 != tri3 || tri2 == tri3 && tri2 != tri1 || tri3 == tri1 && tri2 != tri1 )
 
-  if(tri1 == tri2 && tri1 == tri3)
+    {
+
+      console.log(tri1 + tri2 + tri3);
+      /* Isosceles  */
+      tritype = "isosceles";
+      $(".triangle").html(tritype);
+    }
+
+ else  if(tri1 == tri2 && tri1 == tri3)
   {
     console.log(tri1 + tri2 + tri3);
     /* Equilateral  */
@@ -16,27 +30,13 @@ function calctriangle()
     $(".triangle").html(tritype);
   }
 
-  if(tri1 === tri2 && tri1 != tri3 || tri2 == tri3 && tri2 != tri1)
-
-  {
-
-    console.log(tri1 + tri2 + tri3);
-    /* Isosceles  */
-    tritype = "isosceles";
-    $(".triangle").html(tritype);
-  }
-
-  if(tri1 !== tri2 && tri1 != tri3)
+else if(tri1 !== tri2 && tri1 != tri3 && tri2 != tri3 )
   {
     console.log(tri1 + tri2 + tri3);
     tritype = "scalene";
     $(".triangle").html(tritype);
   }
-if((tri1 + tri2) > tri3 || tri1 > (tri2 + tri3))
-{
-tritype = "not a triangle"
-    $(".triangle").html(tritype);
-}
+
 }
 $(document).ready(function(){
 
